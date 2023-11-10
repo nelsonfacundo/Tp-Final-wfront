731d9522cc348e86ff0ed908c18ee030db366c78
nelsonfacundo <facumati45@hotmail.com>
2023-11-10 01:57:23 -0300

    add react-router-dom for app.js

--- proyect/src/App.js
+++ proyect/src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./components/Home/home";
 
-const App = () => {
+function App() {
   return (
-    <div>
-      <Home />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
-};
+}
 
 export default App;
