df44dab141ca1967fb45a221f9181b40ac671048
Pehuenia <msaguero@gmail.com>
2023-11-26 15:05:30 -0300

    Se agrega funcionalidad de registro de usuario (sin estilos).

--- project/src/App.js
+++ project/src/App.js
@@ -1,17 +1,19 @@
-import React from "react";
-import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./components/home/Home";
-import Login from "./components/login/Login";
-import PetPage from "./components/Pet/PetPage";
+import React from 'react';
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './components/home/Home';
+import Login from './components/login/Login';
+import PetPage from './components/Pet/PetPage';
+import RegisterPage from './components/register/RegisterPage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login/" element={<Login />} />
-        <Route path="/adopt/" element={<PetPage />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/login/' element={<Login />} />
+        <Route path='/adopt/' element={<PetPage />} />
+        <Route path='/register' element={<RegisterPage />} />
       </Routes>
     </BrowserRouter>
   );
--- project/src/components/navbar/Navbar.js
+++ project/src/components/navbar/Navbar.js
@@ -1,33 +1,33 @@
-import React from "react";
-import "../../assets/styles/Navbar.css";
-import Logo from "../../assets/images/Logo.png";
-import { Link } from "react-router-dom";
+import React from 'react';
+import '../../assets/styles/Navbar.css';
+import Logo from '../../assets/images/Logo.png';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <nav className="navbar">
-      <img src={Logo} className="logo" alt="Logo"></img>
-      <ul className="navbar-links">
+    <nav className='navbar'>
+      <img src={Logo} className='logo' alt='Logo'></img>
+      <ul className='navbar-links'>
         <li></li>
         <li>
-          <a href="/">Inicio</a>
+          <a href='/'>Inicio</a>
         </li>
         <li>
-          <a href="/adopt">Adoptar</a>
+          <a href='/adopt'>Adoptar</a>
         </li>
 
         <li>
           <Link to={`/login`}>Inicio de sesión </Link>
         </li>
       </ul>
-      <ul className="navbar-session-user">
+      <ul className='navbar-session-user'>
         <li>
-          <a href="/" className="navbar-register">
+          <Link to={'/register'} className='navbar-register'>
             Registrarse
-          </a>
+          </Link>
         </li>
         <li>
-          <a href="/" className="navbar-session">
+          <a href='/' className='navbar-session'>
             Iniciar Sesion
           </a>
         </li>
