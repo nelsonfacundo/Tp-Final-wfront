afb71d486b3b6d0fc5c0a5c7daff44995b5c6142
Matias Ketlun <matias_ketlun@yahoo.com.ar>
2023-11-26 16:21:59 -0300

    Dog actualizado

--- project/src/components/Dog/dogs.js
+++ project/src/components/Dog/dogs.js
@@ -1,34 +1,24 @@
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Dog = (props) => {
-  const navigate = useNavigate();
-
-  const dogs = {
-    name: props.name.value,
-    specie: props.specie.value,
-    race: props.race.value,
-    gender: props.gender.value,
-    age: props.age.value,
-    description: props.description.value,
-    province: props.province.value,
-  };
-  const requestOptions = {
-    method: "POST",
-    header: { "Content-Type": "application/json" },
-    body: JSON.stringify(dogs),
-  };
-  fetch("localhost:3000/api/pets/", requestOptions)
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("La solicitud no fue exitosa");
-      }
-      navigate("/");
-    })
-    .then((data) => {
-      console.log("Respuesta del servidor:", data);
-    });
-
-  return <div></div>;
+  return (
+    <li>
+      <div>
+        <Link to={`/pets/${props.id}`}>
+          <div></div>
+          <div>
+            <h2>{props.name}</h2>
+            <h3>{props.specie}</h3>
+            <h3>{props.race}</h3>
+            <h3>{props.gender}</h3>
+            <h3>{props.age}</h3>
+            <h3>{props.description}</h3>
+            <h3>{props.province}</h3>
+          </div>
+        </Link>
+      </div>
+    </li>
+  );
 };
 
 export default Dog;
