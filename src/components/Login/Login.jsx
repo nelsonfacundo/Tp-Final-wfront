c85e82faa7c14a5ac1ce7c2e6373ca8ba52421f1
martinrosbier <martinrosbier@gmail.com>
2023-11-27 15:24:29 -0300

    fix: arreglar login
    es headers en vez de header

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -2,28 +2,39 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import Navbar from "../Navigation/Navbar.jsx";
 import Footer from "../Navigation/Footer.jsx";
+import Constants from "../../lib/Constants.js";
 
 const Login = (props) => {
 	const navigate = useNavigate();
 	const LoginHandler = (event) => {
+		event.preventDefault();
 		const login = {
 			email: event.target.Email.value,
 			password: event.target.Password.value,
 		};
 		const requestOptions = {
 			method: "POST",
-			header: { "Content-Type": "application/json" },
+			headers: { "Content-Type": "application/json" },
 			body: JSON.stringify(login),
 		};
-		fetch("localhost:3000/login", requestOptions)
+		fetch(
+			`${Constants.API_BASE_URL}:${Constants.API_PORT}/api/users/login`,
+			requestOptions
+		)
 			.then((response) => {
 				if (!response.ok) {
-					throw new Error("La solicitud no fue exitosa");
+					alert("La solicitud no fue exitosa");
+					throw new Error("Network response was not ok");
 				}
-				navigate("/");
+				return response.json(); // Parse response body as JSON
 			})
 			.then((data) => {
-				console.log("Respuesta del servidor:", data);
+        console.log(data);
+				alert("Respuesta del servidor:", JSON.stringify(data));
+			//	navigate("/");
+			})
+			.catch((error) => {
+				console.error("Error:", error);
 			});
 	};
 
