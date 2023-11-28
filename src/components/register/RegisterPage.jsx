69bbbba722781a6f8875aacb06d1436564775b6e
Pehuenia <msaguero@gmail.com>
2023-11-28 11:23:17 -0300

    Correcciones tipográficas.

--- src/components/register/RegisterPage.jsx
+++ src/components/register/RegisterPage.jsx
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
-import Navbar from '../Navigation/Navbar.jsx';
-import Footer from '../Navigation/Footer.jsx';
-import '../../assets/styles/Register.css';
-import RegisterImage from '../../assets/images/Register_user.png';
-import { useNavigate } from 'react-router-dom';
-import RegisterForm from './RergisterForm.jsx';
+import React, { useState } from "react";
+import Navbar from "../Navigation/Navbar.jsx";
+import Footer from "../Navigation/Footer.jsx";
+import "../../assets/styles/Register.css";
+import RegisterImage from "../../assets/images/Register_user.png";
+import { useNavigate } from "react-router-dom";
+import RegisterForm from "./RegisterForm.jsx";
 
 const RegisterPage = () => {
   const navigate = useNavigate();
@@ -24,33 +24,33 @@ const RegisterPage = () => {
     };
 
     const requestOptions = {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
       body: JSON.stringify(registro),
     };
 
     console.log(
-      'URL de la solicitud:',
-      'http://localhost:5000/api/users/register'
+      "URL de la solicitud:",
+      "http://localhost:3000/api/users/register"
     );
 
-    fetch('http://localhost:5000/api/users/register', requestOptions)
+    fetch("http://localhost:3000/api/users/register", requestOptions)
       .then((response) => {
-        const contentType = response.headers.get('content-type');
-        if (contentType && contentType.includes('application/json')) {
+        const contentType = response.headers.get("content-type");
+        if (contentType && contentType.includes("application/json")) {
           return response.json();
         } else {
           return response.text();
         }
       })
       .then((data) => {
-        console.log('Respuesta del servidor:', data);
+        console.log("Respuesta del servidor:", data);
         setSolicitudEnviada(true);
         setMostrarFormulario(false);
         //navigate('/');
       })
       .catch((error) => {
-        console.error('Error en la solicitud:', error);
+        console.error("Error en la solicitud:", error);
       });
   };
 
@@ -61,10 +61,10 @@ const RegisterPage = () => {
       {mostrarFormulario && <RegisterForm onSubmit={registerHandler} />}
 
       {solicitudEnviada && !mostrarFormulario && (
-        <div className='confirmation-window'>
+        <div className="confirmation-window">
           <p>Tu solicitud fue recibida con éxito</p>
-          <img src={RegisterImage} alt='Imagen de confirmación' />
-          <button className='home-button' onClick={() => navigate('/')}>
+          <img src={RegisterImage} alt="Imagen de confirmación" />
+          <button className="home-button" onClick={() => navigate("/")}>
             Volver al inicio
           </button>
         </div>
