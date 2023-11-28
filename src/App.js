9aa4ce4d873fbb404cfc5fcda086ec4475e7a839
nelsonfacundo <facumati45@hotmail.com>
2023-11-27 21:01:19 -0300

    update  navbar  and component admin

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import React from 'react';
-import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Home from './components/Home/Home';
-import Login from './components/Login/Login';
-import PetPage from './components/Pet/PetPage';
-import AdoptPage from './components/Pet/AdoptPage';
-import RegisterPage from './components/register/RegisterPage';
-import Administrator from './components/administrator/Administrator';
-import AdminAdoptionsPage from './components/Pet/AdminAdoptionsPage';
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./components/Home/Home";
+import Login from "./components/Login/Login";
+import PetPage from "./components/Pet/PetPage";
+import AdoptPage from "./components/Pet/AdoptPage";
+import RegisterPage from "./components/register/RegisterPage";
+import Administrator from "./components/administrator/Administrator";
+import AdminAdoptionsPage from "./components/Pet/AdminAdoptionsPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/login/' element={<Login />} />
-        <Route path='/adopt/' element={<AdoptPage />} />
-        <Route path='/adminadopt/' element={<AdminAdoptionsPage />} />
-        <Route path='/pets/' element={<PetPage />} />
-        <Route path='/register' element={<RegisterPage />} />
-        <Route path='/admin' element={<Administrator />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/login/" element={<Login />} />
+        <Route path="/adopt/" element={<AdoptPage />} />
+        <Route path="/adminadopt/" element={<AdminAdoptionsPage />} />
+        <Route path="/pets/" element={<PetPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/admin" element={<Administrator />} />
+        <Route path="/agregarAdopcion" element={<Administrator />} />
       </Routes>
     </BrowserRouter>
   );
--- src/components/Navigation/Navbar.jsx
+++ src/components/Navigation/Navbar.jsx
@@ -4,68 +4,73 @@ import Logo from "../../assets/images/Logo.png";
 import { Link } from "react-router-dom";
 import Auth, {
   isAdmin,
-	isAuthenticated,
-	getUserFromToken,
-	clearAuthToken,
+  isAuthenticated,
+  getUserFromToken,
+  clearAuthToken,
 } from "../../lib/Auth.js";
 import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
-	const navigate = useNavigate();
+  const navigate = useNavigate();
 
-	const handleLogout = () => {
-		clearAuthToken();
-		navigate("/login");
-	};
+  const handleLogout = () => {
+    clearAuthToken();
+    navigate("/login");
+  };
 
-	return (
-		<nav className="navbar">
-			<img src={Logo} className="logo" alt="Logo"></img>
-			<ul className="navbar-links">
-				<li></li>
-				<li>
-					<a href="/">Inicio</a>
-				</li>
-				{isAuthenticated() ? (
-					<li>
-						<Link to={`/adminadopt`}>Administrar Adopciones</Link>
-					</li>
-				) : (
-					<li>
-						<Link to={`/adopt`}>Adoptar</Link>
-					</li>
-				)}
-				<li>
-					<Link to={`/pets`}>Mascotas</Link>
-				</li>
-			</ul>
-			{isAuthenticated() ? (
-				<ul className="navbar-session-user">
-					<li>
-						<span>
-							Hola {getUserFromToken().firstName} {getUserFromToken().lastName}{" "}
-						</span>
-					</li>
-					<li>
-						<button className="home-button" onClick={handleLogout}>
-							Cerrar sesión
-						</button>
-					</li>
-				</ul>
-			) : (
-				<ul className="navbar-session-user">
-					<li>
-						<Link to={`/register`} className="navbar-register">
-							Registrarse
-						</Link>
-					</li>
-					<li>
-						<Link to={`/login`}>Iniciar sesion</Link>
-					</li>
-				</ul>
-			)}
-		</nav>
-	);
+  return (
+    <nav className="navbar">
+      <img src={Logo} className="logo" alt="Logo"></img>
+      <ul className="navbar-links">
+        <li></li>
+        <li>
+          <a href="/">Inicio</a>
+        </li>
+        {isAuthenticated() && isAdmin() ? (
+          <>
+            <li>
+              <Link to={`/adminadopt`}>Administrar Adopciones</Link>
+            </li>
+            <li>
+              <Link to={`/agregarAdopcion`}>Agregar Mascota</Link>
+            </li>
+          </>
+        ) : (
+          <li>
+            <Link to={`/adopt`}>Adoptar</Link>
+          </li>
+        )}
+        <li>
+          <Link to={`/pets`}>Mascotas</Link>
+        </li>
+      </ul>
+      {isAuthenticated() ? (
+        <ul className="navbar-session-user">
+          <li>
+            <span>
+              Hola {getUserFromToken().firstName} {getUserFromToken().lastName}{" "}
+            </span>
+          </li>
+          <li>
+            <button className="home-button" onClick={handleLogout}>
+              Cerrar sesión
+            </button>
+          </li>
+        </ul>
+      ) : (
+        <ul className="navbar-session-user">
+          <li>
+            <Link to={`/register`} className="navbar-register">
+              Registrarse
+            </Link>
+          </li>
+          <li>
+            <Link to={`/login`}>Iniciar sesion</Link>
+          </li>
+        </ul>
+      )}
+    </nav>
+  );
 };
 
 export default Navbar;
