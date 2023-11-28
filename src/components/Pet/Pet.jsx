ae7d6d2441e905bb2bbf7c252dd865451a2cb7bf
martinrosbier <martinrosbier@gmail.com>
2023-11-27 22:17:50 -0300

    feat: validar segun rol botones en Pet

--- src/components/Pet/Pet.jsx
+++ src/components/Pet/Pet.jsx
@@ -1,128 +1,128 @@
-import React, { useState } from "react";
-import { getUserId } from "../../lib/Auth";
+import React from "react";
+import { getUserId, isAdmin, isAuthenticated } from "../../lib/Auth";
 import "../../assets/styles/Pet.css";
 import Constants from "../../lib/Constants.js";
-import Message from "../Navigation/Message";
 
 const PetCard = ({ pet, showAdoptButton }) => {
-  const [message, setMessage] = useState(""); 
-  const { _id, name, specie, race, gender, age, description, status } = pet;
+	const { _id, name, specie, race, gender, age, description, status } = pet;
 
-  const adoptar = async () => {
-    const userId = getUserId();
-    try {
-      const response = await fetch(
-        `${Constants.API_BASE_URL}:${Constants.API_PORT}/api/adoptions/add-adoption`,
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            petId: _id,
-            adopterId: userId,
-          }),
-        }
-      );
-      if (response.ok) {
-        setMessage({ text:`Solicitud de adopcion enviada`,type: "success"});
-      } else {
-        setMessage({ text:"Solicitud de adopcion fallida",type: "error"});
-      }
-    } catch (error) {
-      console.error("Error: ", error);
-    }
-  };
+	const adoptar = async () => {
+		const userId = getUserId();
+		try {
+			const response = await fetch(
+				`${Constants.API_BASE_URL}:${Constants.API_PORT}/api/adoptions/add-adoption`,
+				{
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json",
+					},
+					body: JSON.stringify({
+						petId: _id,
+						adopterId: userId,
+					}),
+				}
+			);
+			if (response.ok) {
+				alert(`Solicitud de adopcion enviada`);
+			} else {
+				alert("Solicitud de adopcion fallida");
+			}
+		} catch (error) {
+			console.error("Error: ", error);
+		}
+	};
 
-  const rejectAdoption = async () => {
-    try {
-      const response = await fetch(
-        `${Constants.API_BASE_URL}:${Constants.API_PORT}/api/adoptions/reject-adoption/${_id}`,
-        {
-          method: "DELETE",
-        }
-      );
-      if (response.ok) {
-        setMessage({ text:`Adoption rechazada con exito`,type: "success"});
-      } else {
-        setMessage({ text:"Error al rechazar la adopcion",type: "error"});
-      }
-    } catch (error) {
-      console.error("Error: ", error);
-    }
-  };
+	const rejectAdoption = async () => {
+		try {
+			const response = await fetch(
+				`${Constants.API_BASE_URL}:${Constants.API_PORT}/api/adoptions/reject-adoption/${_id}`,
+				{
+					method: "DELETE",
+				}
+			);
+			if (response.ok) {
+				alert(`Adoption rechazada con exito`);
+			} else {
+				alert("Error al rechazar la adopcion");
+			}
+		} catch (error) {
+			console.error("Error: ", error);
+		}
+	};
 
-  const approveAdoption = async () => {
-    try {
-      const response = await fetch(
-        `${Constants.API_BASE_URL}:${Constants.API_PORT}/api/adoptions/approve-adoption/${_id}`,
-        {
-          method: "PUT",
-        }
-      );
-      if (response.ok) {
-        setMessage({ text:`Adopcion realizada con exito`,type: "success"});
-      } else {
-        setMessage({ text:"Adopcion fallida",type: "error"});
-      }
-    } catch (error) {
-      console.error("Error: ", error);
-    }
-  };
+	const approveAdoption = async () => {
+		try {
+			const response = await fetch(
+				`${Constants.API_BASE_URL}:${Constants.API_PORT}/api/adoptions/approve-adoption/${_id}`,
+				{
+					method: "PUT",
+				}
+			);
+			if (response.ok) {
+				alert(`Adopcion realizada con exito`);
+			} else {
+				alert("Adopcion fallida");
+			}
+		} catch (error) {
+			console.error("Error: ", error);
+		}
+	};
 
-  const deleteAdoption = async () => {
-    try {
-      const response = await fetch(
-        `${Constants.API_BASE_URL}:${Constants.API_PORT}/api/adoptions/delete-adoption/${_id}`,
-        {
-          method: "DELETE",
-        }
-      );
-      if (response.ok) {
-        setMessage({ text:`Adopcion borrada con exito`,type: "success"});
-      } else {
-        setMessage({ text:"Fallo el borrar la adopcion",type: "error"});
-      }
-    } catch (error) {
-      console.error("Error: ", error);
-    }
-  };
+	const deleteAdoption = async () => {
+		try {
+			const response = await fetch(
+				`${Constants.API_BASE_URL}:${Constants.API_PORT}/api/adoptions/delete-adoption/${_id}`,
+				{
+					method: "DELETE",
+				}
+			);
+			if (response.ok) {
+				alert(`Adopcion borrada con exito`);
+			} else {
+				alert("Fallo el borrar la adopcion");
+			}
+		} catch (error) {
+			console.error("Error: ", error);
+		}
+	};
 
-  return (
-    <div className="petCard">
-      <h3>{name}</h3>
+	return (
+		<div className="petCard">
+			<h3>{name}</h3>
 
-      <ul>
-        <li>
-          <strong>Especie:</strong> {specie}
-        </li>
-        <li>
-          <strong>Raza:</strong> {race}
-        </li>
-        <li>
-          <strong>Genero:</strong> {gender}
-        </li>
-        <li>
-          <strong>Edad:</strong> {age}
-        </li>
-      </ul>
+			<ul>
+				<li>
+					<strong>Especie:</strong> {specie}
+				</li>
+				<li>
+					<strong>Raza:</strong> {race}
+				</li>
+				<li>
+					<strong>Genero:</strong> {gender}
+				</li>
+				<li>
+					<strong>Edad:</strong> {age}
+				</li>
+			</ul>
 
-      <p>
-        <strong>Descripcion:</strong> {description}
-      </p>
-      <p>
-        <strong>Estado de adopcion:</strong> {status}
-      </p>
-      {showAdoptButton && (
-        <>
-          <button onClick={() => adoptar(_id)}>Adoptar</button>
-          <button onClick={rejectAdoption}>Rechazar</button>
-          <button onClick={approveAdoption}>Aprobar</button>
-          <button onClick={deleteAdoption}>Eliminar</button>
-        </>
-      )}
-    </div>
-  );
+			<p>
+				<strong>Descripcion:</strong> {description}
+			</p>
+			<p>
+				<strong>Estado de adopcion:</strong> {status}
+			</p>
+			{showAdoptButton && isAuthenticated() && isAdmin() && (
+				<>
+					<button onClick={rejectAdoption}>Rechazar</button>
+					<button onClick={approveAdoption}>Aprobar</button>
+					<button onClick={deleteAdoption}>Eliminar</button>
+				</>
+			)}
+			{showAdoptButton && isAuthenticated() && !isAdmin() && (
+				<button onClick={() => adoptar(_id)}>Adoptar</button>
+			)}
+		</div>
+	);
 };
 
 export default PetCard;
